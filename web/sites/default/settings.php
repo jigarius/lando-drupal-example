e0b2c36c00dbc61a23df0841b79bf0273608a69a
Jigar Mehta <jigarius@gmail.com>
2020-06-17 22:32:17 -0400

    Installing drupal/core 9.0.1.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -251,9 +251,7 @@ $databases = [];
  *   ];
  * @endcode
  */
-$config_directories = [
-  CONFIG_SYNC_DIRECTORY => '../config/sync',
-];
+$settings['config_sync_directory'] = '../config/sync';
 
 /**
  * Settings:
@@ -778,4 +776,3 @@ $settings['entity_update_backup'] = TRUE;
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
-$settings['install_profile'] = 'standard';
